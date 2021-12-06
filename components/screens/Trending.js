bb7cd4c15b1eb974619c4fa29577305de956646b
deje0014 <deje0014@algonquinlive.com>
2021-12-06 12:14:53 -0500

    Removed unused code / style from Trending

--- components/screens/Trending.js
+++ components/screens/Trending.js
@@ -73,50 +73,6 @@ export default function Trending() {
 
 
 function Shows({shows}){
-//data coming from "shows"
-// title = shows.item['title']
-// year = shows.item['year']
-// ids =  shows.item['ids'] (imdb / slug / tmdb / trakt)
-
-//images / fetching data from TMDB
-//tmdb = a1b2f514b71b98f4fdeabd6fae26bd24
-//IMG_URL: 'https://image.tmdb.org/t/p/'
-//img = APP.IMG_URL + 'w500' + obj.poster_path;
-
-  // const [img, setImg] = useState('');
-  // function getImage(id){
-  //     let api_key = 'a1b2f514b71b98f4fdeabd6fae26bd24';
-  //     let url = `https://api.themoviedb.org/3/tv/${id}?api_key=${api_key}`;
-  //     fetch(url)
-  //       .then((resp)=>{
-  //         if (!resp.ok) throw new Error(resp.statusText);
-  //           return resp.json();
-  //       })
-  //       .then((data) => {
-  //         //console.log(data.backdrop_path)
-  //         return setImg(data.poster_path)
-  //       })
-  //       .catch(console.error);
-  //     }
-  // getImage(shows.item['ids'].tmdb);
-
-  // let imgURL = `https://image.tmdb.org/t/p/w500${img}`;
-
-  // return (
-  //   <View style={styles.card}>
-  //     <Pressable  
-  //       style={styles.likeBtn}
-  //       onPress={(ev)=>{
-  //       console.log(`you pressed ${shows.item['ids'].tmdb}` )
-  //     }}>
-  //     <Icon name='heart' type='evilicon' color='pink' iconProps={{size:30}}/>
-  //     </Pressable>
-  //     <Image style={styles.image} source={{uri: imgURL}} />
-  //     <Text style={styles.title}> {shows.item['title']}</Text> 
-  //     <Text style={styles.released_year}> {shows.item['year']}</Text>
-  //   </View>
-  // )
-
   return (
     <List shows={shows} />
   )
@@ -137,34 +93,9 @@ const styles = StyleSheet.create({
     marginLeft:15,
     paddingTop: 15
   },
-  card:{
-    alignSelf:"flex-start",
-    flexShrink: 1,
-    width:(width/3)-20,
-  },
-  image: {
-    width:(width/3)-20,
-    height:(width/2)-20,
-    borderRadius:7,
-    marginTop:20
-  },
-  title:{
-    fontSize:13,
-    color:"#fff"
-  },
-  likeBtn:{
-    position:"absolute",
-    right: 3, 
-    top: 25, 
-    zIndex: 10 ,
-  },
   briefInfo:{
     flexWrap:"wrap",
     flexDirection:"row",
-  },
-  released_year:{
-    fontSize:11,
-    color:"gray"
   },
   loading:{
     color:"#000",
