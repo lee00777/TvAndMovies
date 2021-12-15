3c0903ab7564338fa3a41dbea8d020c96dfb0659
deje0014 <deje0014@algonquinlive.com>
2021-12-15 13:48:54 -0500

    Updated Details style

--- components/screens/Details.js
+++ components/screens/Details.js
@@ -2,9 +2,10 @@ import { ScrollView } from 'native-base';
 import React, {useEffect, useState} from 'react'
 import { View, Text, StyleSheet, Dimensions, ActivityIndicator} from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Button, Card, Header, Icon } from 'react-native-elements';
+import { Button, Card, Header, Icon, Image } from 'react-native-elements';
 
 const width =  Dimensions.get('window').width;
+const height =  Dimensions.get('window').height;
 
 export default function Details({navigation, route}) {
   //TODO: fix JSON Parse error: Unexpected EOF
@@ -32,9 +33,10 @@ export default function Details({navigation, route}) {
             return resp.json();
         })
         .then((data) => {
+          console.log(data);
             setShow(data);
             getImage(data['ids'].tmdb);
-            getNextEpisode(show['ids'].trakt);
+            getNextEpisode(data['ids'].trakt);
         })
         .catch((error) => {
             console.log(error)
@@ -81,28 +83,27 @@ export default function Details({navigation, route}) {
           })
           .catch(console.error);
         };
-    let imgURL = `https://image.tmdb.org/t/p/w500${img}`;
+    let imgURL = `https://image.tmdb.org/t/p/w500/${img}`;
     
     return (
         <SafeAreaProvider style={styles.safeArea}>
           <ScrollView>
-            <Card containerStyle={{fontFamily: 'roboto', borderRadius: 7}}>
-              <Card.Title style={styles.title}>{show.title}</Card.Title>
-              <Card.Image title={show.title} PlaceholderContent={<ActivityIndicator size="large" color="#00ff00" />} style={styles.image} source={{uri: imgURL}} />
+            <View style={styles.card}>
+              <Text style={styles.title}>{show.title}</Text>
+              <Image title={show.title} PlaceholderContent={<ActivityIndicator size="large" color="#00ff00" />} style={styles.image} source={{uri: imgURL}} />
               {/* <Text style={styles.title} >Overview</Text> */}
               <Text style={styles.text} >{show.overview}</Text>
-              <Card.Divider/>
-              <Text style={styles.text}>First air date: {new Date(show.first_aired).toLocaleDateString()}</Text>
-              <Text style={styles.text}>Network: {show.network} / {show.country}</Text>
-              <Text style={styles.text}>Current status: {show.status}</Text>
-              <Text style={styles.text}>Total Episodes: {show.aired_episodes}</Text>
-              <Card.Divider/>
+              <View style={styles.basicInfo}>
+              <Text style={styles.basicInfoText}>First air date: {new Date(show.first_aired).toLocaleDateString()}</Text>
+              <Text style={styles.basicInfoText}>Network: {show.network} / {show.country}</Text>
+              <Text style={styles.basicInfoText}>Current status: {show.status}</Text>
+              <Text style={styles.basicInfoText}>Total Episodes: {show.aired_episodes}</Text>
+              </View>
               
               <View style={styles.nextEp}>
                 {
-                  console.log(nextEpisode),
                   nextEpisode ? <>
-                  <Card.FeaturedTitle  style={styles.featTitle}>Next Episode</Card.FeaturedTitle>
+                  <Text  style={styles.featTitle}>Next Episode</Text>
                   <Text  style={styles.text}>Title: {nextEpisode.title ? nextEpisode.title : "Not Available"} </Text>
                   <Text  style={styles.text}>Date: {nextEpisode.first_aired ? new Date(nextEpisode.first_aired).toLocaleDateString() : "Not Available"}</Text>
                   </>
@@ -111,11 +112,10 @@ export default function Details({navigation, route}) {
                   </>
                 }
             </View>
-              <Card.Divider/>
               <Button title="Back" titleStyle={{fontWeight: 'bold'}} onPress={()=> navigation.goBack()} icon={
             <Icon name='arrowleft' type='antdesign' size={25} color='white'/>
           } />
-              </Card>
+              </View>
           </ScrollView>
         </SafeAreaProvider>
     )
@@ -128,23 +128,43 @@ const styles = StyleSheet.create({
     backgroundColor:'#202124',
     justifyContent: 'flex-start',
   },
+  card:{
+    borderRadius: 7,
+    margin: 5
+  },
   image:{
     borderRadius: 20,
-    alignContent: 'center',
+    alignSelf: 'center',
     width:(width/2)-20,
     height:(width)-20,
-    marginBottom: 10
+    margin: 10
+  },
+  nextEp: {
+    marginVertical: 5,
   },
   title: {
+    margin: 10,
     fontSize: 25,
     fontWeight: 'bold',
-    color: 'black',
-  },
-  featTitle: {
-    color: 'black',
+    color: 'white',
     textAlign: 'center'
   },
-  text: {
+  featTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  basicInfo: {
+    margin: 5,
+  },
+  basicInfoText:{
+    color: '#fff',
     textTransform: 'capitalize'
+  },
+  text: {
+    color: '#fff',
+    textAlign: 'justify',
+    padding: 5
   }
 })
