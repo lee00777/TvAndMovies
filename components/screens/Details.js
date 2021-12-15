43a1390bdef5e6e8bb4262807cc2e239b194ec9e
deje0014 <deje0014@algonquinlive.com>
2021-12-15 13:22:06 -0500

    Added conditional rendering in Details

--- components/screens/Details.js
+++ components/screens/Details.js
@@ -5,7 +5,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Button, Card, Header, Icon } from 'react-native-elements';
 
 const width =  Dimensions.get('window').width;
-const height =  Dimensions.get('window').height;
 
 export default function Details({navigation, route}) {
   //TODO: fix JSON Parse error: Unexpected EOF
@@ -35,7 +34,7 @@ export default function Details({navigation, route}) {
         .then((data) => {
             setShow(data);
             getImage(data['ids'].tmdb);
-            getNextEpisode(data['ids'].trakt);
+            getNextEpisode(show['ids'].trakt);
         })
         .catch((error) => {
             console.log(error)
@@ -83,13 +82,11 @@ export default function Details({navigation, route}) {
           .catch(console.error);
         };
     let imgURL = `https://image.tmdb.org/t/p/w500${img}`;
-
-    const activityIndicator = <ActivityIndicator />
     
     return (
         <SafeAreaProvider style={styles.safeArea}>
           <ScrollView>
-            <Card containerStyle={{fontFamily: 'roboto', borderRadius: 7,     height: (height)}}>
+            <Card containerStyle={{fontFamily: 'roboto', borderRadius: 7}}>
               <Card.Title style={styles.title}>{show.title}</Card.Title>
               <Card.Image title={show.title} PlaceholderContent={<ActivityIndicator size="large" color="#00ff00" />} style={styles.image} source={{uri: imgURL}} />
               {/* <Text style={styles.title} >Overview</Text> */}
@@ -99,13 +96,21 @@ export default function Details({navigation, route}) {
               <Text style={styles.text}>Network: {show.network} / {show.country}</Text>
               <Text style={styles.text}>Current status: {show.status}</Text>
               <Text style={styles.text}>Total Episodes: {show.aired_episodes}</Text>
-              <Card.Divider/>{
-                nextEpisode.title && nextEpisode.first_aired &&
+              <Card.Divider/>
+              
               <View style={styles.nextEp}>
+                {
+                  console.log(nextEpisode),
+                  nextEpisode ? <>
                   <Card.FeaturedTitle  style={styles.featTitle}>Next Episode</Card.FeaturedTitle>
-                  <Text  style={styles.text}>Title: {nextEpisode.title} </Text>
-                  <Text  style={styles.text}>Date: {new Date(nextEpisode.first_aired).toLocaleDateString()}</Text>
-              </View>}
+                  <Text  style={styles.text}>Title: {nextEpisode.title ? nextEpisode.title : "Not Available"} </Text>
+                  <Text  style={styles.text}>Date: {nextEpisode.first_aired ? new Date(nextEpisode.first_aired).toLocaleDateString() : "Not Available"}</Text>
+                  </>
+                  :
+                  <>
+                  </>
+                }
+            </View>
               <Card.Divider/>
               <Button title="Back" titleStyle={{fontWeight: 'bold'}} onPress={()=> navigation.goBack()} icon={
             <Icon name='arrowleft' type='antdesign' size={25} color='white'/>
