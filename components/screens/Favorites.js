fa3813ad6e9c86db4d35bd21840b093d9f1ddca9
GYUYOUNG LEE <lee00777@algonquinlive.com>
2021-12-15 20:01:50 -0500

    Added no favorite item msg to favorites component

--- components/screens/Favorites.js
+++ components/screens/Favorites.js
@@ -1,39 +1,55 @@
-import React, {useContext} from 'react'
 import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native'
+import React, {useState, useEffect, useContext} from 'react'
 import GlobalContext from '../utils/globalContext.utils.';
 import List from '../List';
+import * as StoreReview from 'expo-store-review';
 
 export default function Favorites() {
-    const {faveData} = useContext(GlobalContext);
+  const {faveData} = useContext(GlobalContext);
 
+  useEffect(() => {
+    if(faveData.length >= 1){
+      StoreReview.requestReview();
+    }
+    // setTimeout(()=>{
+    //   StoreReview.requestReview()
+    // },5000)
+  }, [])
 
-    return (
-        <SafeAreaView style={styles.safeArea} edges={['right', 'bottom', 'left']}>
-               <Text style={styles.header}>Favorites</Text>
-               <FlatList 
-              data={faveData}
-              numColumns={3}
-              columnWrapperStyle={{flex:1, justifyContent:"space-around"}}
-              renderItem={(item)=>( <List shows={item} />)}
-            keyExtractor={item => item.key}
-          />
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={styles.safeArea} edges={['right', 'bottom', 'left']}>
+      <Text style={styles.header}>Favorites</Text>
+      <FlatList 
+        data={faveData}
+        numColumns={3}
+        columnWrapperStyle={{flex:1, justifyContent:"space-around"}}
+        renderItem={(item)=>( <List shows={item} />)}
+        ListEmptyComponent={<Text style={styles.noFavItemMsg}> No Favorite Item </Text>}
+        keyExtractor={item => item.key}
+      />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-    safeArea:{
-        flex: 1,  
-        resizeMode: 'center', 
-        backgroundColor:'#202124',
-        justifyContent: 'flex-start',
-      },
-      header:{
-        fontFamily:"roboto",
-        fontSize: 20,
-        color:"#fff",
-        textAlign:'left',
-        marginLeft:15,
-        paddingTop: 15
-      },
+  safeArea:{
+    flex: 1,  
+    resizeMode: 'center', 
+    backgroundColor:'#202124',
+    justifyContent: 'flex-start',
+  },
+  header:{
+    fontFamily:"roboto",
+    fontSize: 20,
+    color:"#fff",
+    textAlign:'left',
+    marginLeft:15,
+    paddingTop: 15
+  },
+  noFavItemMsg:{
+    color:"#fff",
+    fontSize: 18,
+    textAlign:'center',
+    marginTop:100
+  }
 })
